c0ae8e7fbd4e43f9a1a337bf3b555dd71808499b
highlander1337 <tassioleno@gmail.com>
2025-06-22 18:21:19 -0300

    Add in-memory-bookly fastapi project (Without sql)

---
+++ python-fastapi-projects/in-memory-bookly/src/books/books.py
@@ -0,0 +1,93 @@
+
+books = [
+    {
+        "id": 1,
+        "title": "1984",
+        "author": "George Orwell",
+        "publisher": "Secker & Warburg",
+        "published_date": "1949-06-08",
+        "page_count": 328,
+        "language": "en"
+    },
+    {
+        "id": 2,
+        "title": "To Kill a Mockingbird",
+        "author": "Harper Lee",
+        "publisher": "J.B. Lippincott & Co.",
+        "published_date": "1960-07-11",
+        "page_count": 281,
+        "language": "en"
+    },
+    {
+        "id": 3,
+        "title": "One Hundred Years of Solitude",
+        "author": "Gabriel García Márquez",
+        "publisher": "Harper & Row",
+        "published_date": "1970-03-05",
+        "page_count": 417,
+        "language": "en"
+    },
+    {
+        "id": 4,
+        "title": "The Catcher in the Rye",
+        "author": "J.D. Salinger",
+        "publisher": "Little, Brown and Company",
+        "published_date": "1951-07-16",
+        "page_count": 234,
+        "language": "en"
+    },
+    {
+        "id": 5,
+        "title": "The Hobbit",
+        "author": "J.R.R. Tolkien",
+        "publisher": "George Allen & Unwin",
+        "published_date": "1937-09-21",
+        "page_count": 310,
+        "language": "en"
+    },
+    {
+        "id": 6,
+        "title": "Pride and Prejudice",
+        "author": "Jane Austen",
+        "publisher": "T. Egerton, Whitehall",
+        "published_date": "1813-01-28",
+        "page_count": 279,
+        "language": "en"
+    },
+    {
+        "id": 7,
+        "title": "The Divine Comedy",
+        "author": "Dante Alighieri",
+        "publisher": "John Murray",
+        "published_date": "1320",
+        "page_count": 798,
+        "language": "it"
+    },
+    {
+        "id": 8,
+        "title": "The Brothers Karamazov",
+        "author": "Fyodor Dostoevsky",
+        "publisher": "The Russian Messenger",
+        "published_date": "1880",
+        "page_count": 824,
+        "language": "ru"
+    },
+    {
+        "id": 9,
+        "title": "The Little Prince",
+        "author": "Antoine de Saint-Exupéry",
+        "publisher": "Reynal & Hitchcock",
+        "published_date": "1943-04-06",
+        "page_count": 96,
+        "language": "fr"
+    },
+    {
+        "id": 10,
+        "title": "Brave New World",
+        "author": "Aldous Huxley",
+        "publisher": "Chatto & Windus",
+        "published_date": "1932-01-01",
+        "page_count": 311,
+        "language": "en"
+    }
+]
